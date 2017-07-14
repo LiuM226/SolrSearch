147b69826aeccb511fdf6ed92fe8d3e82093658c
guolf <guolingfa@foxmail.com>
2017-07-14 20:17:57 +0800

    开发分支，使用MQ/Dubbo

--- search/src/main/java/com/jxtii/solr/config/JmsConfig.java
+++ search/src/main/java/com/jxtii/solr/config/JmsConfig.java
@@ -1,35 +1,35 @@
-//package com.jxtii.solr.config;
-//
-//import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-//import org.springframework.jms.config.JmsListenerContainerFactory;
-//import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-//import org.springframework.jms.support.converter.MessageConverter;
-//import org.springframework.jms.support.converter.MessageType;
-//
-//import javax.jms.ConnectionFactory;
-//
-///**
-// * Created by guolf on 17/7/4.
-// */
-//@Configuration
-//public class JmsConfig {
-//
-//    @Bean
-//    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
-//                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        configurer.configure(factory, connectionFactory);
-//        return factory;
-//    }
-//
-//    @Bean
-//    public MessageConverter jacksonJmsMessageConverter() {
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setTargetType(MessageType.TEXT);
-//        converter.setTypeIdPropertyName("_type");
-//        return converter;
-//    }
-//}
+package com.jxtii.solr.config;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+/**
+ * Created by guolf on 17/7/4.
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
--- search/src/main/java/com/jxtii/solr/mq/Receiver.java
+++ search/src/main/java/com/jxtii/solr/mq/Receiver.java
@@ -1,30 +1,30 @@
-//package com.jxtii.solr.mq;
-//
-//import com.jxtii.solr.entity.SolrContent;
-//import com.jxtii.solr.entity.SolrResult;
-//import com.jxtii.solr.service.SolrService;
-//import com.jxtii.solr.utils.MQConstant;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jms.annotation.JmsListener;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * Created by guolf on 17/7/3.
-// */
-//@Component
-//public class Receiver {
-//
-//    @Autowired
-//    private SolrService solrService;
-//
-//    @JmsListener(destination = MQConstant.MQ_RECEIVE_INDEX, containerFactory = "myFactory")
-//    public void receiveCreateIndexMessage(SolrContent Content) {
-//        System.out.println("Received <" + Content + ">");
-//        solrService.createIndexWithFile(Content);
-//    }
-//
-//    @JmsListener(destination = MQConstant.MQ_RECEIVE_RESULT, containerFactory = "myFactory")
-//    public void receiveResultMessage(SolrResult result) {
-//        System.out.println("Received <" + result + ">");
-//    }
-//}
+package com.jxtii.solr.mq;
+
+import com.jxtii.solr.entity.SolrContent;
+import com.jxtii.solr.entity.SolrResult;
+import com.jxtii.solr.service.SolrService;
+import com.jxtii.solr.utils.MQConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by guolf on 17/7/3.
+ */
+@Component
+public class Receiver {
+
+    @Autowired
+    private SolrService solrService;
+
+    @JmsListener(destination = MQConstant.MQ_RECEIVE_INDEX, containerFactory = "myFactory")
+    public void receiveCreateIndexMessage(SolrContent Content) {
+        System.out.println("Received <" + Content + ">");
+        solrService.createIndexWithFile(Content);
+    }
+
+    @JmsListener(destination = MQConstant.MQ_RECEIVE_RESULT, containerFactory = "myFactory")
+    public void receiveResultMessage(SolrResult result) {
+        System.out.println("Received <" + result + ">");
+    }
+}
